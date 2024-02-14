821b07ce665c9a5f229ead67bac5cc77acb730a6
nick-urzagasti <nicolas.urzagasti@mercadolibre.com>
2024-02-14 15:31:21 -0300

    primer commit

---
+++ src/main/java/com/bootcamp/ejercicio_link_tracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.bootcamp.ejercicio_link_tracker.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
